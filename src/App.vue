4cb916d2130ae0e71ca66994a2acab7f3b76f89e
laynexx <argo150808@gmail.com>
2025-04-13 19:04:36 +0300

    updated images

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ onMounted(async ()=>{
         </div>
         <div class="qr-code">
           <a href="http://qrcoder.ru" target="_blank">
-            <img src="http://qrcoder.ru/code/?https%3A%2F%2Farthur-izmailyan.duckdns.org%2Flyceum-6%2Fmain&4&0" alt="main qr" title="QR код">
+            <img src="/qr1.png" alt="main qr" title="QR код">
           </a>
         </div>
       </div>
@@ -43,7 +43,7 @@ onMounted(async ()=>{
         </div>
         <div class="qr-code">
           <a href="http://qrcoder.ru" target="_blank">
-            <a href="http://qrcoder.ru" target="_blank"><img src="http://qrcoder.ru/code/?https%3A%2F%2Farthur-izmailyan.duckdns.org%2Flyceum-6%2Fstudent-rules&4&0" width="164" height="164" border="0" title="QR код"></a>
+            <img src="/qr2.png" alt="main qr" title="QR код">
           </a>
         </div>
       </div>
@@ -56,7 +56,7 @@ onMounted(async ()=>{
         </div>
         <div class="qr-code">
           <a href="http://qrcoder.ru" target="_blank">
-            <a href="http://qrcoder.ru" target="_blank"><img src="http://qrcoder.ru/code/?https%3A%2F%2Farthur-izmailyan.duckdns.org%2Flyceum-6%2Flyceum-regulation&4&0" width="180" height="180" border="0" title="QR код"></a>
+            <img src="/qr3.png" alt="main qr" title="QR код">
           </a>
         </div>
       </div>
